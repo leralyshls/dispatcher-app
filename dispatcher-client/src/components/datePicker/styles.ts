a1f4c0d2da3b93b05256736ef33fbb7a65805ae2
lera <>
2022-05-17 11:28:31 +0300

    added shared style to date-picker

--- dispatcher-client/src/components/datePicker/styles.ts
+++ dispatcher-client/src/components/datePicker/styles.ts
@@ -1,16 +1,11 @@
 import styled from 'styled-components';
-import { COLORS } from '../../utils/colors';
 import { FlexRow } from '../../styles/sharedStyles';
+import { filterSharedStyles } from '../../styles/sharedStyles';
 
 export const DatesFilterContainer = styled(FlexRow)`
-  font-family: Roboto, sans-serif;
-  font-size: 0.875rem;
-  box-sizing: border-box;
+  ${filterSharedStyles}
   min-height: 3.36em;
   max-width: 10.93rem;
-  background: ${COLORS.white};
-  border: none;
   border-radius: 0.625rem;
-  padding: 0.93em 1.2em 0.93em 0.93em;
-  cursor: pointer;
+  padding: 0.93em 1.25em 0.93em 0.93em;
 `;
